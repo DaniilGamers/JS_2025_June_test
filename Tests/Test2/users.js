6d6e108b386d535d37941d39d3e0ac835362d157
DaniilGamers <ss.mihalkivd@gmail.com>
2025-06-30 20:36:03 +0200

    Import previous test and fix

--- Tests/Test2/users.js
+++ Tests/Test2/users.js
@@ -3,23 +3,27 @@
 // 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
 // 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
 
-
-
 fetch('http://jsonplaceholder.typicode.com/users')
   .then(res => res.json())
   .then(users => {
 
+    let userList = document.createElement('div')
+
+    document.body.appendChild(userList)
+
+    userList.classList.add("usersList")
+
+
     let divUsers = document.createElement('div');
 
     document.body.appendChild(divUsers);
-    divUsers.classList.add('users');
     for (const user of users) {
       //console.log(users)
 
 
       let objects = document.createElement('div')
 
-      objects.classList.add('ObjectBlock')
+      objects.classList.add("ObjectBlock")
 
       let h3 = document.createElement('h2')
 
@@ -42,6 +46,8 @@ fetch('http://jsonplaceholder.typicode.com/users')
       objects.append(h3)
       objects.appendChild(linkText)
       divUsers.appendChild(objects)
+      userList.append(divUsers)
 
     }
+
   });
