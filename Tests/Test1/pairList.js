fc91b80718c7357531b4c79af9215ab3df9fee29
DaniilGamers <ss.mihalkivd@gmail.com>
2025-07-01 16:34:46 +0200

    2nd Day update - Create pairList.js file for index.html file

---
+++ Tests/Test1/pairList.js
@@ -0,0 +1,48 @@
+//////Pair array//////
+
+function pairList(name, value) {
+    this.name = name
+    this.value = value
+}
+
+//////Add pair to the list storage/////
+
+
+
+function addPair (id, name, value) {
+
+
+
+    const input = document.getElementById('inputBox')
+
+    if (input === ''){
+
+        console.log("Wrong")
+
+    }else {
+
+        console.log("Work!")
+
+    }
+
+}
+
+/////Show pair on window list/////
+
+
+
+
+
+/////Sort by Name function/////
+
+
+
+/////Sort by Value function/////
+
+
+
+/////Delete function/////
+
+function deletePair() {
+    
+}
