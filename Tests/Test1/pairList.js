4378a15ff3a5f59193ccd7030d805d7ca0d69f06
DaniilGamers <ss.mihalkivd@gmail.com>
2025-07-02 02:42:56 +0200

    3rd Day - Create list of phrases and local storage and console test

--- Tests/Test1/pairList.js
+++ Tests/Test1/pairList.js
@@ -1,35 +1,84 @@
 //////Pair array//////
 
-function pairList(name, value) {
-    this.name = name
-    this.value = value
-}
+let id = 0
 
+let list = JSON.parse(localStorage.getItem('pairs'))
+
+if (list === null){
+
+    console.log('list is empty')
+
+}else{
+
+console.log(list)
+
+}
 //////Add pair to the list storage/////
 
+function addPair () {
+
+    const test = document.getElementById('inputBox')
+
+    const text = test.value
+
+    if (!text.includes('=') || text.startsWith('=') || text.endsWith('=') || !text === undefined){
+
+        console.log('Write something or put between Name and Value with symbol =')
+
+    }else{
+
+        const [name, value] = text.split('=')
 
 
-function addPair (id, name, value) {
+        const pairList = {
 
+            id: id,
+            name: name,
+            value: value,
 
+        };
 
-    const input = document.getElementById('inputBox')
+        if (pairList.name === list.name && pairList.value === list.value){
 
-    if (input === ''){
+            console.log('Come up with something original!')
 
-        console.log("Wrong")
+        }else{
 
-    }else {
+            list.push(pairList)
 
-        console.log("Work!")
+            console.log(list)
 
+            localStorage.setItem('pairs', JSON.stringify(list));
+        }
     }
 
+    id++;
 }
 
 /////Show pair on window list/////
 
 
+if (list === null){
+
+    console.log('list is empty')
+
+}else{
+
+    for (const pair of list){
+
+        let pairListText = document.getElementById('div')
+
+        document.body.appendChild(pairListText)
+
+        pairListText.classList.add('pairListText')
+
+        pairListText.innerHTML = `<div>${pair.name}=${pair.value}</div>`
+
+        console.log(`${pair.name}=${pair.value}`)
+
+    }
+
+}
 
 
 
